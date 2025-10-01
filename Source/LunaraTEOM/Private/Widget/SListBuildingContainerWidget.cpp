ab0cfe0623e8ac822c1a4abf2b8d5482e8412f9e
teuku <teukusalmanfarizi2003@gmail.com>
2025-10-01 23:15:51 +0700

    Add SListBuildingContainerWidget class and initial implementation

---
+++ Source/LunaraTEOM/Private/Widget/SListBuildingContainerWidget.cpp
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Widget/SListBuildingContainerWidget.h"
+#include "SlateOptMacros.h"
+
+BEGIN_SLATE_FUNCTION_BUILD_OPTIMIZATION
+void SListBuildingContainerWidget::Construct(const FArguments& InArgs)
+{
+	/*
+	ChildSlot
+	[
+		// Populate the widget
+	];
+	*/
+}
+END_SLATE_FUNCTION_BUILD_OPTIMIZATION
